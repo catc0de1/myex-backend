72c16237bee285e3322329c398e35aba293c3760
Iyan Zuli Armanda <catcode0101@gmail.com>
2025-09-07 01:06:55 +0700

    feat: add controller method for create user & pipe validate using class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import { DataSource } from 'typeorm';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,13 @@ async function bootstrap() {
   } else {
     console.error('Database connection failed');
   }
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(process.env.PORT ?? 3000);
 }
