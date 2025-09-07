e77609984393a7dcce8b8fa603dcbed562cb1774
Iyan Zuli Armanda <catcode0101@gmail.com>
2025-09-07 20:09:36 +0700

    feat: add controller and dto to update data

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,10 +5,12 @@ import {
   Post,
   Param,
   Query,
+  Patch,
   Delete,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { FindUserDto } from './dtos/find-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -28,6 +30,11 @@ export class UsersController {
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 
   @Delete('/:id')
