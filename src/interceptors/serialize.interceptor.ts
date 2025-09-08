511363723d2449ede2ef7c13d3cab340bf71aed6
Iyan Zuli Armanda <catcode0101@gmail.com>
2025-09-08 15:24:31 +0700

    perf: add type safety in interceptors

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -9,22 +9,31 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToInstance } from 'class-transformer';
 
-export function Serialize(dto: any) {
+type ClassConstructor<T> = {
+  new (...args: any[]): T;
+};
+
+// if you want to restrict to only object types
+// interface ClassConstructor {
+//   new (...args: any[]): object;
+// }
+
+export function Serialize<T>(dto: ClassConstructor<T>) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
 @Injectable()
-export class SerializeInterceptor implements NestInterceptor {
-  constructor(private dto: any) {}
+export class SerializeInterceptor<T> implements NestInterceptor {
+  constructor(private dto: ClassConstructor<T>) {}
 
   intercept(
     context: ExecutionContext,
-    next: CallHandler<any>,
-  ): Observable<any> | Promise<Observable<any>> {
+    next: CallHandler<T>,
+  ): Observable<T | T[]> | Promise<Observable<T>> {
     // Run something before a request is handled by the request handler
     // console.log('Running before the handler', context);
     return next.handle().pipe(
-      map((data: any) => {
+      map((data: T | T[]) => {
         // Run something before the response is sent out or after the request is handled by the request handler
         // console.log('Running after the handler', data);
 
