2a4d1f6906d27f2bb2bebdb0326623915782b598
CatC0de1 <catcode0101@gmail.com>
2025-09-22 18:20:02 +0700

    feat: add feature to create new item with auth required

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ItemsService } from '@/items/items.service';
+import { CreateItemDto } from '@/items/dtos/create-item.dto';
+import { Item } from '@/items/item.entity';
+import { AuthGuard } from '@/guard/auth.guard';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  constructor(private itemsService: ItemsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  createItem(@Body() body: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(body);
+  }
+}
